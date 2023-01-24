24dc9b8c5b71ccf660efac32af3fed8e5d54022f
KevRiver <kanggakky1214@gmail.com>
2023-01-25 08:46:19 +0900

    TempMember로 DB IO 구현 중

--- backend/rif/src/main/java/team/a501/rif/domain/achievement/AchievementAcq.java
+++ backend/rif/src/main/java/team/a501/rif/domain/achievement/AchievementAcq.java
@@ -1,6 +1,7 @@
 package team.a501.rif.domain.achievement;
 
 import team.a501.rif.domain.member.Member;
+import team.a501.rif.domain.tmp.TempMember;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -16,10 +17,45 @@ public class AchievementAcq {
 
     private Boolean onDisplay;
 
+//    @ManyToOne
+//    private Member member; // 멤버가 삭제되면 업적획득도 사라져야한다
+
     @ManyToOne
-    private Member member; // 멤버가 삭제되면 업적획득도 사라져야한다
+    private TempMember member; // todo TempMember로 테스트 후 다시 Member로 변경하기
 
     // todo 업적이 삭제 되더라도 멤버가 삭제되지 않았다면 Null로 대체 되거나 디폴트 값으로 변경?
     @ManyToOne
     private Achievement achievement;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Boolean getOnDisplay() {
+        return onDisplay;
+    }
+
+    public void setOnDisplay(Boolean onDisplay) {
+        this.onDisplay = onDisplay;
+    }
+
+    public TempMember getMember() {
+        return member;
+    }
+
+    public void setMember(TempMember member) {
+        this.member = member;
+    }
+
+    public Achievement getAchievement() {
+        return achievement;
+    }
+
+    public void setAchievement(Achievement achievement) {
+        this.achievement = achievement;
+    }
 }
--- backend/rif/src/main/java/team/a501/rif/domain/badge/BadgeAcq.java
+++ backend/rif/src/main/java/team/a501/rif/domain/badge/BadgeAcq.java
@@ -1,6 +1,7 @@
 package team.a501.rif.domain.badge;
 
 import team.a501.rif.domain.member.Member;
+import team.a501.rif.domain.tmp.TempMember;
 
 import javax.persistence.*;
 
@@ -13,23 +14,42 @@ public class BadgeAcq {
 
     private Boolean onDisplay;
 
+//    @ManyToOne
+//    private Member member; // 멤버가 삭제되면 뱃지획득 역시 삭제되어야한다
+
     @ManyToOne
-    private Member member; // 멤버가 삭제되면 뱃지획득 역시 삭제되어야한다
+    private TempMember member; // todo 테스트용
 
     // todo 뱃지가 삭제되어도 멤버가 남아있다면 뱃지획득이 삭제되어선 안된다. 뱃지가 삭제되면 기본 뱃지로 대체되는 것도 고려해보자
     @ManyToOne
     private Badge badge;
 
-    public Member getMember() {
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Boolean getOnDisplay() {
+        return onDisplay;
+    }
+
+    public void setOnDisplay(Boolean onDisplay) {
+        this.onDisplay = onDisplay;
+    }
+
+    public TempMember getMember() {
         return member;
+    }
+
+    public void setMember(TempMember member) {
+        this.member = member;
     }
 
     public Badge getBadge() {
         return badge;
-    }
-
-    public void setMember(Member member) {
-        this.member = member;
     }
 
     public void setBadge(Badge badge) {
