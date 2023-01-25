b862507f83ef5d47c263005d67e76f32a1637dc8
veonico <veonico@naver.com>
2023-01-25 17:14:48 +0900

    [feat]LotComponent added

--- frontend/rif/src/Pages/DescriptionPageComponentTwo.js
+++ frontend/rif/src/Pages/DescriptionPageComponentTwo.js
@@ -1,37 +1,49 @@
-import { Grid, Container } from '@mui/material'
-import BtnComponent from '../UI/BtnComponent'
-import { Link } from 'react-router-dom'
-import PageChangerComponent from '../UI/PageChangerComponent'
+import { Grid, Container } from "@mui/material";
+import BtnComponent from "../UI/BtnComponent";
+import { Link } from "react-router-dom";
+import PageChangerComponent from "../UI/PageChangerComponent";
 
-const DescriptionPageComponentTwo = (() => {
-    return (
-            <div>
-                <Link to ="/home" style={{textDecoration:'none'}}>
-                    <PageChangerComponent> 메인화면 </PageChangerComponent>
-                </Link>
+const DescriptionPageComponentTwo = () => {
+  return (
+    <div>
+      <PageChangerComponent to="/home"> 메인화면 </PageChangerComponent>
+      <Grid
+        container
+        className="grid-container"
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        sx={{ my: "2rem" }}
+      >
+        <Grid item className="grid-header">
+          <img
+            src="/rif-logo-96.png"
+            alt="rif logo 96"
+            className="rif-logo-96"
+          />
+        </Grid>
+        <Grid item className="grid-body">
+          <div className="rif-stands-for" style={{ color: "#5C855D" }}>
+            <h1>사용법</h1>
+          </div>
+        </Grid>
+        <Grid item className="grid-description">
+          <Container className="description" sx={{ mx: "auto" }}>
+            <p> 대충 사용하시면 됩니다 ㅎㅎ</p>
+          </Container>
+        </Grid>
+        <Grid item className="grid-buttons">
+          <Link to="/description/1" style={{ textDecoration: "none" }}>
+            <BtnComponent color="secondary">이전 : 왜 RIF인가? </BtnComponent>
+          </Link>
+          <br />
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <BtnComponent> 서비스 시작하기 </BtnComponent>
+          </Link>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 
-                <Grid container className="grid-container" direction="column" alignItems="center" justifyContent="center" sx={{ my : "2rem"}}>
-                    <Grid item className="grid-header">
-                        <img src="/rif-logo-96.png" alt="rif logo 96" className="rif-logo-96" />
-                    </Grid>
-                    <Grid item className="grid-body">
-                        <div className="rif-stands-for" style={{color:"#5C855D"}}>
-                            <h1>사용법</h1>
-                        </div>
-                    </Grid>
-                    <Grid item className="grid-description">
-                        <Container className="description" sx={{mx:'auto'}}>
-                            <p> 대충 사용하시면 됩니다 ㅎㅎ</p>
-                        </Container>
-                    </Grid>
-                    <Grid item className="grid-buttons">
-                        <Link to ="/description/1" style={{textDecoration:'none'}}> <BtnComponent color='secondary'> 이전 : 왜 RIF인가? </BtnComponent> </Link>
-                        <br/>
-                        <Link to ="/" style={{textDecoration:'none'}}> <BtnComponent> 서비스 시작하기 </BtnComponent> </Link>
-                    </Grid>
-                </Grid>
-            </div>
-    )
-})
-
-export default DescriptionPageComponentTwo
+export default DescriptionPageComponentTwo;
--- frontend/rif/src/Router.js
+++ frontend/rif/src/Router.js
@@ -1,37 +1,43 @@
-import { createBrowserRouter } from 'react-router-dom'
-// import ErrorPageComponent from './Pages/ErrorPageComponent'
-// import LoginPageComponent from './Pages/LoginPageComponent'
-import HomePageComponent from './Pages/HomePageComponent'
-import DescriptionPageComponent from './Pages/DescriptionPageComponent';
-import DescriptionPageComponentTwo from './Pages/DescriptionPageComponentTwo';
-import App from './App';
-
+import { createBrowserRouter } from "react-router-dom";
+import ErrorPageComponent from "./Pages/ErrorPageComponent";
+import LoginPageComponent from "./Pages/LoginPageComponent";
+import HomePageComponent from "./Pages/HomePageComponent";
+import DescriptionPageComponent from "./Pages/DescriptionPageComponent";
+import DescriptionPageComponentTwo from "./Pages/DescriptionPageComponentTwo";
+import LotComponent from "./Pages/LotComponent";
+import App from "./App";
 
 const router = createBrowserRouter([
-    {  // main page
-        path: "/",
-        element: <App />,
-        // errorElement : <ErrorPageComponent />
-    },
-    // {  // LoginPage
-    //     path: "/login",
-    //     element : <LoginPageComponent />,
-    // },
-    {
-        // HomePage
-        path : "/home",
-        element : <HomePageComponent />,
-    },
-    {
-        // Description Page (why RIF?)
-        path : "/description/1",
-        element : <DescriptionPageComponent />,
-    },
-    {
-        // Description Page Two (How to Use)
-        path : "/description/2",
-        element : <DescriptionPageComponentTwo />,
-    },
-])
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPageComponent />,
+  },
+  {
+    // LoginPage
+    path: "/login",
+    element: <LoginPageComponent />,
+  },
+  {
+    // HomePage
+    path: "/home",
+    element: <HomePageComponent />,
+  },
+  {
+    // Description Page (why RIF?)
+    path: "/description/1",
+    element: <DescriptionPageComponent />,
+  },
+  {
+    // Description Page Two (How to Use)
+    path: "/description/2",
+    element: <DescriptionPageComponentTwo />,
+  },
+  {
+    // Lotto Component
+    path: "/lot",
+    element: <LotComponent />,
+  },
+]);
 
 export default router;
