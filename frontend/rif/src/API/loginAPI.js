08d12e1549095a26361ec1bbd9eaf6d8d595acdc
vinitus <tlsdnrng@gmail.com>
2023-01-26 12:26:59 +0900

    refactoring

--- frontend/rif/src/API/loginAPI.js
+++ frontend/rif/src/API/loginAPI.js
@@ -1,17 +1,15 @@
 import axiosInterface from "./axiosInterface"
 
-/** response status code in RIF is 200 or 401.
- * return is Destructuring assignment. First element is response data object, second element status code integer.
+/** Response status code in RIF is 200 or 401.
  */
-const loginAPI = (id, password) => {
-  const response = axiosInterface("get", "user/login", { id, password })
-  let status
+async function loginAPI(id, password) {
+  const response = await axiosInterface("post", "user/login", { id, password })
+
   if (response.status === 200) {
-    status = 200
-  } else {
-    status = response.status
+    return response
   }
-  return [response, status]
+
+  return response.response
 }
 
 export default loginAPI
