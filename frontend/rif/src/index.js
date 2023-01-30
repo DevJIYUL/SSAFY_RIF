6be938ba8e095e90a31af31e9c810576e8457c06
vinitus <tlsdnrng@gmail.com>
2023-01-30 13:16:48 +0900

    [feat] redux provider 추가

--- frontend/rif/src/index.js
+++ frontend/rif/src/index.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import router from './Router'
-import './index.css';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import router from "./Router"
+import "./index.css"
 // import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { RouterProvider } from 'react-router-dom';
+import reportWebVitals from "./reportWebVitals"
+import { RouterProvider } from "react-router-dom"
+import { Provider } from "react-redux"
+import store from "./store/index"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
