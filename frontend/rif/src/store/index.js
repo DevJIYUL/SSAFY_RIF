16be08337cad9fb290abafabb6bed995d5f4bbb0
vinitus <tlsdnrng@gmail.com>
2023-01-31 12:09:26 +0900

    [feat] userSearchSlice add on store

--- frontend/rif/src/store/index.js
+++ frontend/rif/src/store/index.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit"
 import UISlice from "./UISlice"
 import authSlice from "./auth"
 import userInfoSlice from "./getUserInfo"
+import searchResultSlice from "./searchResultSlice"
 
 const store = configureStore({
   reducer: {
     ui: UISlice.reducer,
     auth: authSlice.reducer,
     user: userInfoSlice.reducer,
+    search: searchResultSlice.reducer,
   },
 })
 
