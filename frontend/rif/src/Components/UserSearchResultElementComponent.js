ca6f2457bd7f39bd570bd5a9231b5d8ce6a9c36e
vinitus <tlsdnrng@gmail.com>
2023-01-31 12:18:26 +0900

    [feat] make show user info when user searched

---
+++ frontend/rif/src/Components/UserSearchResultElementComponent.js
@@ -0,0 +1,14 @@
+const UserSearchResultElementComponent = (props) => {
+  return (
+    <div>
+      {Object.entries(props.recentSearchResult).map(([key, value]) => (
+        <p key={props.recentSearchResult.id + key}>
+          {key}:{value}
+        </p>
+      ))}
+      <hr />
+    </div>
+  )
+}
+
+export default UserSearchResultElementComponent
