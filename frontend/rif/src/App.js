cc3b8f3aa43c92e37df49297bcc5e6d777f2b497
vinitus <tlsdnrng@gmail.com>
2023-02-02 09:09:59 +0900

    [refactor] use createBrowseRrotuer and RouterProvider

--- frontend/rif/src/App.js
+++ frontend/rif/src/App.js
@@ -1,18 +1,86 @@
-import BtnComponent from "./UI/BtnComponent"
-import { Link } from "react-router-dom"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import LoginPageComponent from "./Pages/LoginPageComponent";
+import MainPageComponent from "./Pages/MainPageComponent";
+import HomePageComponent from "./Pages/HomePageComponent";
+import DescriptionPageComponent from "./Pages/DescriptionPageComponent";
+import DescriptionPageComponentTwo from "./Pages/DescriptionPageComponentTwo";
+import ErrorPageComponent from "./Pages/ErrorPageComponent";
+import LotPageComponent from "./Pages/LotPageComponent";
+import UserSearchComponent from "./Pages/UserSearchComponent";
+import BadgePageComponent from "./Pages/BadgePageComponent";
+import RankingPageComponent from "./Pages/RankingPageComponent";
+import ChangeProfilePageComponent from "./Pages/ChangeProfilePageComponent";
+import ChangePasswordPageComponent from "./Pages/ChangePasswordPageComponent";
+import RootLayOut from "./UI/RootLayOut";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayOut />,
+    errorElement: <ErrorPageComponent />,
+    children: [
+      {
+        // LoginPage
+        path: "/login",
+        element: <LoginPageComponent />,
+      },
+      {
+        // HomePage
+        path: "/home",
+        element: <HomePageComponent />,
+      },
+      {
+        // MainPage
+        path: "/index",
+        element: <MainPageComponent />,
+      },
+      {
+        // Description Page (why RIF?)
+        path: "/description/1",
+        element: <DescriptionPageComponent />,
+      },
+      {
+        // Description Page Two (How to Use)
+        path: "/description/2",
+        element: <DescriptionPageComponentTwo />,
+      },
+      {
+        // Lotto Component
+        path: "/lot",
+        element: <LotPageComponent />,
+      },
+      {
+        // User Search Component
+        path: "/search",
+        element: <UserSearchComponent />,
+      },
+      {
+        // sectiontitle test
+        path: "/badge",
+        element: <BadgePageComponent />,
+      },
+      {
+        // Ranking Page Component
+        path: "/ranking",
+        element: <RankingPageComponent />,
+      },
+      {
+        // Change Profile Component
+        path: "/change-profile",
+        element: <ChangeProfilePageComponent />,
+      },
+      {
+        // Change Password Page Component
+        path: "/change-password",
+        element: <ChangePasswordPageComponent />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <div>
-      <h1>app.js</h1>
-      <Link to="/lot" style={{ textDecoration: "none" }}>
-        <BtnComponent> 로또 컴포넌트 </BtnComponent>
-      </Link>
-      <Link to="/login" style={{ textDecoration: "none" }}>
-        <BtnComponent> 로그인 컴포넌트 </BtnComponent>
-      </Link>
-    </div>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
