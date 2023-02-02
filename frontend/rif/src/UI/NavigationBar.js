07a1c1d6334761beb146d57077ba93d107d05467
vinitus <tlsdnrng@gmail.com>
2023-02-02 09:09:14 +0900

    [feat] add css and ul, li tag

--- frontend/rif/src/UI/NavigationBar.js
+++ frontend/rif/src/UI/NavigationBar.js
@@ -1,4 +1,5 @@
 // import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const NavigationBar = () => {
   // const navigate = useNavigate();
@@ -7,18 +8,28 @@ const NavigationBar = () => {
     console.log(data);
   };
   return (
-    <nav
-      style={{ width: "100%", display: "flex", justifyContent: "space-evenly" }}
-    >
-      {["search", "ranking", "home", "main", "lot"].map((value, index) => (
-        <img
-          src={`navigationbar/${value}.png`}
-          alt={value}
-          key={`navigation${index}`}
-          data-navigation={value}
-          onClick={navigationBarClickHandler}
-        />
-      ))}
+    <nav>
+      <ul
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "space-evenly",
+        }}
+      >
+        {["search", "ranking", "home", "main", "lot"].map((value, index) => (
+          <li>
+            <Link to={`/${value}`}>
+              <img
+                src={`navigationbar/${value}.png`}
+                alt={value}
+                key={`navigation${index}`}
+                data-navigation={value}
+                onClick={navigationBarClickHandler}
+              />
+            </Link>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 };
