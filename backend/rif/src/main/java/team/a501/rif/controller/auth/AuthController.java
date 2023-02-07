08841847b03505842f8dc7815b08d0bd2cb8e3ab
kev <kanggakky1214@gmail.com>
2023-02-07 14:29:35 +0900

    [feat] AchievementController - 업적을 생성할 수 있다 - 전체 업적을 조회할 수 있다 - Controller 레이어에서 Domain 레이어의 클래스를 참조하지 않도록 코드를 리팩토링 하였다

--- backend/rif/src/main/java/team/a501/rif/controller/auth/AuthController.java
+++ backend/rif/src/main/java/team/a501/rif/controller/auth/AuthController.java
@@ -4,12 +4,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import team.a501.rif.dto.auth.TokenDto;
-
 import team.a501.rif.dto.auth.LoginRequest;
+import team.a501.rif.dto.auth.TokenDto;
 import team.a501.rif.service.auth.AuthService;
 
 @RestController
--- backend/rif/src/main/java/team/a501/rif/service/badge/BadgeService.java
+++ backend/rif/src/main/java/team/a501/rif/service/badge/BadgeService.java
@@ -4,6 +4,8 @@ import team.a501.rif.domain.badge.Badge;
 import team.a501.rif.dto.badge.BadgeInfo;
 import team.a501.rif.dto.badge.BadgeSaveRequest;
 
+import java.util.List;
+
 public interface BadgeService {
 
     Badge save(BadgeSaveRequest badge);
@@ -11,6 +13,8 @@ public interface BadgeService {
     Badge findById(Long id);
 
     Badge getRandomBadge();
+
+    List<Badge> findAll();
 
     void deleteById(Long id);
 
