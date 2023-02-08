c59035cab1627eddd34171e0710bc9a1045002ab
vinitus <tlsdnrng@gmail.com>
2023-02-08 17:52:00 +0900

    [feat] add style

--- frontend/rif/src/Pages/ChangeProfilePageComponent.js
+++ frontend/rif/src/Pages/ChangeProfilePageComponent.js
@@ -3,6 +3,9 @@ import { useSelector, useDispatch } from "react-redux";
 import { mainPageRequestHandler } from "../store/getUserInfo";
 import changeProfileInfoAPI from "../API/changeProfileInfoAPI";
 import { useNavigate } from "react-router-dom";
+import { Grid } from "@mui/material";
+import SectionTitleComponent from "../UI/SectionTitleComponent";
+import PageChangerComponent from "../UI/PageChangerComponent";
 
 const ChangeProfilePageComponent = () => {
   const navigate = useNavigate();
@@ -12,7 +15,6 @@ const ChangeProfilePageComponent = () => {
   const profileImgpath = useSelector(
     (state) => state.user.userInfo.profileImgPath
   );
-  const nickname = useSelector((state) => state.user.userInfo.nickname);
 
   useEffect(() => {
     if (!profileImgpath) {
@@ -21,25 +23,16 @@ const ChangeProfilePageComponent = () => {
   }, [profileImgpath, dispatch]);
 
   const [selectedProfileImg, setselectedProfileImg] = useState(profileImgpath);
-  const [newNickname, setNewNickname] = useState(nickname);
 
   const profileClickHandler = (event) => {
     setselectedProfileImg(event.target.dataset.profileId);
   };
 
-  const nicknameInputChangeHandler = (event) => {
-    setNewNickname(event.target.value);
-  };
-
   const changeProfileSubmitHandler = (event) => {
     event.preventDefault();
-    async function changeProfileRequest(profileImgPathId, nickname, token) {
+    async function changeProfileRequest(profileImgPathId, token) {
       try {
-        const response = await changeProfileInfoAPI(
-          profileImgPathId,
-          nickname,
-          token
-        );
+        const response = await changeProfileInfoAPI(profileImgPathId, token);
 
         if (response.status !== 200) {
           throw new Error("Error is comming.");
@@ -49,32 +42,53 @@ const ChangeProfilePageComponent = () => {
         console.log(error);
       }
     }
-    changeProfileRequest(selectedProfileImg, newNickname, token);
+    changeProfileRequest(selectedProfileImg, token);
   };
 
   return (
-    <div>
-      <p>ChangeProfileComponent</p>
-      <form action="" onSubmit={changeProfileSubmitHandler}>
-        {[1, 2, 3, 4, 5, 6].map((value, index) => (
-          <img
-            src={`profile/${value}.png`}
-            alt="loading"
-            data-profile-id={value}
-            key={`profile${index}`}
-            onClick={profileClickHandler}
-          ></img>
-        ))}
-        <input
-          type="text"
-          name="nickname"
-          id="nickname"
-          value={newNickname}
-          onChange={nicknameInputChangeHandler}
-        />
-        <button type="submit">완료!</button>
-      </form>
-    </div>
+    <>
+      <PageChangerComponent to="/main">메인 화면</PageChangerComponent>
+      <Grid container>
+        <SectionTitleComponent sectionTitle="프로필 사진 수정"></SectionTitleComponent>
+        <form
+          action=""
+          onSubmit={changeProfileSubmitHandler}
+          style={{
+            width: "90%",
+          }}
+        >
+          <Grid>
+            {[1, 4].map((firstValue) => (
+              <Grid
+                container
+                justifyContent="space-evenly"
+                key={`changeProfileGrid${firstValue}`}
+                style={{
+                  marginTop: "20px",
+                }}
+              >
+                {[0, 1, 2].map((value, index) => (
+                  <img
+                    src={`profile/${value + firstValue}.png`}
+                    alt="loading"
+                    data-profile-id={value + firstValue}
+                    key={`profile${index + firstValue}`}
+                    style={{
+                      minHeight: "70px",
+                      height: "25vw",
+                      borderRadius: "50%",
+                      margin: "3px 0px",
+                      border: "3px solid #4E9E00",
+                    }}
+                    onClick={profileClickHandler}
+                  />
+                ))}
+              </Grid>
+            ))}
+          </Grid>
+        </form>
+      </Grid>
+    </>
   );
 };
 
