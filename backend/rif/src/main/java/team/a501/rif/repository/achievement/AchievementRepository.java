04b28658494225c82d5ad685792bb58d41fe7a97
veonico <veonico@naver.com>
2023-02-09 15:52:06 +0900

    [fix] drop backend (have no idea when I pulled it :( )

--- backend/rif/src/main/java/team/a501/rif/repository/achievement/AchievementRepository.java
+++
@@ -1,7 +0,0 @@
-package team.a501.rif.repository.achievement;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import team.a501.rif.domain.achievement.Achievement;
-
-public interface AchievementRepository extends JpaRepository<Achievement, Long> {
-}
--- backend/rif/src/main/java/team/a501/rif/service/auth/AuthService.java
+++
@@ -1,8 +0,0 @@
-package team.a501.rif.service.auth;
-
-import team.a501.rif.domain.auth.Token;
-
-public interface AuthService {
-
-    Token login(String studentId,String password) throws RuntimeException;
-}
