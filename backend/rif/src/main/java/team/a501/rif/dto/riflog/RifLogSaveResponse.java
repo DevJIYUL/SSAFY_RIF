44054e71e4dfb107cf22c73ba124a88b43824e61
kev <kanggakky1214@gmail.com>
2023-02-10 16:16:25 +0900

    [fix] RifLogSaveResponse, RifLogInfo updated - RifLogInfo, RifLogSaveResponse에 exp, point 필드를 추가하였다 - MemberServiceImpl.addRifLog: rifLogService.save 메서드에 exp, point 파라미터 추가

--- backend/rif/src/main/java/team/a501/rif/dto/riflog/RifLogSaveResponse.java
+++ backend/rif/src/main/java/team/a501/rif/dto/riflog/RifLogSaveResponse.java
@@ -11,13 +11,21 @@ import java.time.LocalDateTime;
 public class RifLogSaveResponse {
 
     private String name;
+    private Integer plasticTotal;
+    private Integer plasticOk;
+    private Integer recycleTotal;
+    private Integer recycleOk;
     private Integer point;
     private Integer exp;
     private LocalDateTime createdAt;
 
     @Builder
-    public RifLogSaveResponse(String name, Integer point, Integer exp, LocalDateTime createdAt) {
+    public RifLogSaveResponse(String name, Integer plasticTotal, Integer plasticOk, Integer recycleTotal, Integer recycleOk, Integer point, Integer exp, LocalDateTime createdAt) {
         this.name = name;
+        this.plasticTotal = plasticTotal;
+        this.plasticOk = plasticOk;
+        this.recycleTotal = recycleTotal;
+        this.recycleOk = recycleOk;
         this.point = point;
         this.exp = exp;
         this.createdAt = createdAt;
