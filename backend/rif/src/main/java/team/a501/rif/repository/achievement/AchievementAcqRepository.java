b76e0e21db51c22ca7a21b1e5d9f090814ce4c92
KevRiver <kanggakky1214@gmail.com>
2023-02-12 02:53:21 +0900

    [chore] AchievementAcqRepository Updated - imports have been optimized

--- backend/rif/src/main/java/team/a501/rif/repository/achievement/AchievementAcqRepository.java
+++ backend/rif/src/main/java/team/a501/rif/repository/achievement/AchievementAcqRepository.java
@@ -2,9 +2,6 @@ package team.a501.rif.repository.achievement;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.a501.rif.domain.achievement.AchievementAcq;
-import team.a501.rif.domain.member.Member;
-
-import java.util.List;
 
 public interface AchievementAcqRepository extends JpaRepository<AchievementAcq, Long> {
 }
