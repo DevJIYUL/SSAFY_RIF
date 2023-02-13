8d9859563482a78696a232f08e5dd05cc6656fde
vinitus <tlsdnrng@gmail.com>
2023-02-14 08:52:44 +0900

    [feat] remove trailing slash, add rediect about pathname / to /home

--- frontend/rif/src/UI/RootLayOut.js
+++ frontend/rif/src/UI/RootLayOut.js
@@ -1,7 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate, useLocation } from "react-router-dom";
 import NavigationComponent from "./NavigationComponent";
+import { useEffect } from "react";
 
 const RootLayOut = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  console.log(location);
+  useEffect(() => {
+    if (location.pathname === "/") {
+      navigate("/home");
+    } else if (location.pathname.endsWith("/")) {
+      navigate(location.pathname.slice(0, -1));
+    }
+  }, [navigate, location]);
+
   return (
     <>
       <main style={{ marginBottom: "70px" }}>
