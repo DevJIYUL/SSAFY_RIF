cc6f8f55629b920f15781f9346be877148870e01
ytjin101 <ytjin101@gmail.com>
2023-02-13 18:01:22 +0900

    [fix] RFID값 str으로 넘기기

--- embedded/qtMain.py
+++ embedded/qtMain.py
@@ -20,26 +20,25 @@ import socket
 # 수거하면서 data server로 전송
 
 # item(eng)
-item_eng = {'clear plastic cup lid': 0, 'clear plastic cup' : 1, 'colored plastic cup lid' : 2,
-        'inked plastic cup' : 3, 'litter' : 4, 'paper cup holder' : 5,
-        'paper cup with cup holder' : 6, 'paper cup' : 7,
-        'plastic bottle cap' : 8, 'plastic bottle' : 9,
-        'plastic cup with cup holder' : 10,
-        'straw' : 11, 'white plastic cup lid' : 12,
-        'label' : 13, 'plastic bottle labeled' : 14}
+item_eng = {'clear plastic cup lid': 0, 'clear plastic cup': 1, 'colored plastic cup lid': 2,
+            'inked plastic cup': 3, 'litter': 4, 'paper cup holder': 5,
+            'paper cup with cup holder': 6, 'paper cup': 7,
+            'plastic bottle cap': 8, 'plastic bottle': 9,
+            'plastic cup with cup holder': 10,
+            'straw': 11, 'white plastic cup lid': 12,
+            'label': 13, 'plastic bottle labeled': 14}
 
 # item(kor)
-item_kor = ['투명 플라스틱 뚜껑', '투명 플라스틱 컵', '유색 플라스틱 뚜껑', 
-        '인쇄된 플라스틱 컵', '라벨지', '종이 컵홀더', '종이컵 및 홀더',
-        '종이컵', '페트병 뚜껑', '페트병', '플라스틱컵 및 홀더',
-        '빨대', '흰색 플라스틱 뚜껑', '라벨지', '라벨 있는 페트병']
-
+item_kor = ['투명 플라스틱 뚜껑', '투명 플라스틱 컵', '유색 플라스틱 뚜껑',
+            '인쇄된 플라스틱 컵', '라벨지', '종이 컵홀더', '종이컵 및 홀더',
+            '종이컵', '페트병 뚜껑', '페트병', '플라스틱컵 및 홀더',
+            '빨대', '흰색 플라스틱 뚜껑', '라벨지', '라벨 있는 페트병']
 
 # plastic : P, recycle : R, trash : T, 재분류 : X
 trash_label = ['R', 'R', 'P', 'P', 'T', 'R', 'X', 'T', 'P', 'R', 'X', 'T', 'P', 'P', 'X']
 
-sensor_num = 1 # 1 : RFID, 2 : distance sensor
-toggle_flag = 0 # 1 : toggle (guide 1, 2)
+sensor_num = 1  # 1 : RFID, 2 : distance sensor
+toggle_flag = 0  # 1 : toggle (guide 1, 2)
 wrongFlag = 0
 socketFlag = 0
 show_wrong_cnt = 0
@@ -55,9 +54,10 @@ user_uid = 'xxxxxxxx'
 
 wrong_list = ["", "", ""]
 
+
 class MyApp(QWidget, Ui_Form):
-     
     servo_pwm = 0
+
     def __init__(self):
         global sensor_num
 
@@ -69,9 +69,9 @@ class MyApp(QWidget, Ui_Form):
 
         # buzzer
         self.buzzer = 21
-        
+
         GPIO.setmode(GPIO.BCM)
-        
+
         # buzzer
         GPIO.setup(self.buzzer, GPIO.OUT)
 
@@ -105,7 +105,7 @@ class MyApp(QWidget, Ui_Form):
 
         GPIO.add_event_detect(self.button1, GPIO.BOTH, callback=self.btn1Pressed, bouncetime=500)
         GPIO.add_event_detect(self.button2, GPIO.BOTH, callback=self.btn2Pressed, bouncetime=500)
-        
+
         # servo trashcan
         self.servo = 22
         GPIO.setup(self.servo, GPIO.OUT)
@@ -117,25 +117,25 @@ class MyApp(QWidget, Ui_Form):
         self.tm_showWrong = QTimer()
         self.tm_showWrong.setInterval(2000)
         self.tm_showWrong.timeout.connect(self.showWrong)
-        
+
         # qt timer askAgain
         self.tm_askAgain = QTimer()
         self.tm_askAgain.setInterval(50)
         self.tm_askAgain.timeout.connect(self.askAgain)
-        
+
         # qt timer collect
         self.tm_collect = QTimer()
         self.tm_collect.setInterval(50)
         self.tm_collect.timeout.connect(self.collectGarbage)
-        
+
         # qt timer showPoint
         self.tm_showPoints = QTimer()
         self.tm_showPoints.setInterval(50)
         self.tm_showPoints.timeout.connect(self.showPoints)
 
-        #qt timer showFirst
+        # qt timer showFirst
         self.tm_showFirst = QTimer()
-        self.tm_showFirst.setInterval(2000)
+        self.tm_showFirst.setInterval(4000)
         self.tm_showFirst.timeout.connect(self.showFirst)
 
     def main(self):
@@ -166,7 +166,7 @@ class MyApp(QWidget, Ui_Form):
             if GPIO.input(3):
                 pass
             else:
-                toggle_flag = 0    
+                toggle_flag = 0
                 self.setupImageNum(5)
                 socketFlag = 1
         elif self.arr_idx == 8:
@@ -184,21 +184,20 @@ class MyApp(QWidget, Ui_Form):
                 self.setupImageNum(9)
                 time.sleep(2)
                 self.collectGarbage()
-    
-    def switchDisplay(self, val):
-        global toggle_flag, user_uid 
 
-        print("RFID val : " + hex(val))
-        
+    def switchDisplay(self, val):
+        global toggle_flag, user_uid
+
+        print("RFID val : " + val)
+
         # switch display
-        if val: # val is not null
-            uid_str = str(hex(val))
+        if val:  # val is not null
+            uid_str = val
             uid_str = uid_str[2:]
             user_uid = uid_str
 
-        
             r = requests.get('https://i8a501.p.ssafy.io/api/member', params={'uid': uid_str}, verify=False)
-            
+
             if r.status_code != 200:
                 print("Connect ERROR")
             else:
@@ -212,7 +211,6 @@ class MyApp(QWidget, Ui_Form):
                 self.switch_display(json_info['name'])
                 time.sleep(2)
                 toggle_flag = 1
-                
 
     def toggleDisplay(self, val):
         if val == 1:
@@ -221,7 +219,7 @@ class MyApp(QWidget, Ui_Form):
     # 분류하는 함수 / 후에 yolo detect 함수 들어가면 됨
     def detection(self, val):
         global user_uid, wrongFlag, sensor_num, socketFlag
-        
+
         # logout
         if val == 0:
             print("empty logout")
@@ -240,13 +238,13 @@ class MyApp(QWidget, Ui_Form):
                 print("Wrong first time")
                 wrongFlag = 1
                 # 버릴지 다시 할지 선택하기
-            
+
                 self.setupImageNum(7)
                 self.tm_showWrong.start()
-                #time.sleep(2)
-                #self.showWrong()
-                #self.setupImageNum(8)
-            
+                # time.sleep(2)
+                # self.showWrong()
+                # self.setupImageNum(8)
+
             # 두 번 틀렸으면 수거
             elif wrongFlag == 1:
                 wrongFlag = 2
@@ -259,8 +257,8 @@ class MyApp(QWidget, Ui_Form):
             self.setupImageNum(6)
             time.sleep(2)
             self.tm_collect.start()
-    
-    # 틀린 항목 보여주는 함수 
+
+    # 틀린 항목 보여주는 함수
     def showWrong(self):
         global wrong_list, show_wrong_cnt, wrongFlag
 
@@ -278,8 +276,7 @@ class MyApp(QWidget, Ui_Form):
         self.wrong_list_text.setText(wrong_list[show_wrong_cnt])
         print(wrong_list[show_wrong_cnt])
         show_wrong_cnt += 1
-        
-        
+
     def askAgain(self):
         if self.tm_askAgain.isActive() == True:
             self.tm_askAgain.stop()
@@ -307,7 +304,7 @@ class MyApp(QWidget, Ui_Form):
     # 쓰레기 수거 후 로그아웃 화면
     def showPoints(self):
         global user_uid, sensor_num, wrongFlag, plasticTotal, plasticOk, recycleTotal, recycleOk
-        
+
         yoontae_flag = 0
 
         if self.tm_showPoints.isActive() == True:
@@ -315,19 +312,19 @@ class MyApp(QWidget, Ui_Form):
             yoontae_flag = 1
 
         # 수거 정보 데이터 전송
-        #print("UID " + user_uid + ' ' + str(plasticTotal) + ' ' + str(plasticOk) + ' ' + str(recycleTotal) + ' ' + str(recycleOk)) 
-        #requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
-        #urllib3.disable_warnings()
+        # print("UID " + user_uid + ' ' + str(plasticTotal) + ' ' + str(plasticOk) + ' ' + str(recycleTotal) + ' ' + str(recycleOk))
+        # requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+        # urllib3.disable_warnings()
         headers = {'Content-Type': "application/json; charset=utf-8"}
-        datas = {'uid' : str(user_uid),
-            'plasticTotal' : str(plasticTotal), 'plasticOk' : str(plasticOk),
-            'recycleTotal' : str(recycleTotal), 'recycleOk' : str(recycleOk)}
-        print("===================server send data=======================") 
+        datas = {'uid': str(user_uid),
+                 'plasticTotal': str(plasticTotal), 'plasticOk': str(plasticOk),
+                 'recycleTotal': str(recycleTotal), 'recycleOk': str(recycleOk)}
+        print("===================server send data=======================")
         print(headers)
         print(datas)
 
-        res = requests.post('http://i8a501.p.ssafy.io:8080/api/member/riflog', headers=headers, json=datas, verify=False)
-    
+        res = requests.post('https://i8a501.p.ssafy.io/api/member/riflog', headers=headers, json=datas, verify=False)
+
         print(res.json())
 
         if res.status_code != 200:
@@ -335,20 +332,19 @@ class MyApp(QWidget, Ui_Form):
         else:
             recycle_json = res.json()
             print("NAME : ", recycle_json['name'])
-            print("POINT : " , recycle_json['point'])
+            print("POINT : ", recycle_json['point'])
             print("EXP : ", recycle_json['exp'])
-
 
         if yoontae_flag == 1:
             self.setupImageNum(10)
             self.soogoe_name.setText(recycle_json['name'])
             self.soogoe_score.setText(str(recycle_json['point']))
-            #time.sleep(3)
-            #self.soogoe_name.setText("")
-            #self.soogoe_score.setText("")
+            # time.sleep(3)
+            # self.soogoe_name.setText("")
+            # self.soogoe_score.setText("")
             self.tm_showFirst.start()
             return
-        
+
         # 점수 데이터 보여주기
         self.setupImageNum(10)
         self.soogoe_name.setText(recycle_json['name'])
@@ -356,7 +352,7 @@ class MyApp(QWidget, Ui_Form):
         time.sleep(3)
         self.soogoe_name.setText("")
         self.soogoe_score.setText("")
-        
+
         # logout
         user_uid = ''
         sensor_num = 1
@@ -366,17 +362,17 @@ class MyApp(QWidget, Ui_Form):
             self.tm_showFirst.start()
             return
         self.setupImageNum(0)
-    
+
     def showFirst(self):
         global user_uid, sensor_num, wrongFlag
 
         if self.tm_showFirst.isActive() == True:
             self.tm_showFirst.stop()
-        #self.soogoe_name.setText("")
-        #self.soogoe_score.setText("")
+        # self.soogoe_name.setText("")
+        # self.soogoe_score.setText("")
         self.soogoe_name.setText("")
         self.soogoe_score.setText("")
-        
+
         user_uid = ''
         sensor_num = 1
         wrongFlag = 0
@@ -386,10 +382,10 @@ class MyApp(QWidget, Ui_Form):
 
 # RFID, distance sensor thread
 class MyThread(QThread):
-
     distanceSignal = Signal(int)
     mySignal = Signal(int)
     buttonSignal = Signal(int)
+
     def __init__(self):
         super().__init__()
         # RFID sensor
@@ -397,11 +393,11 @@ class MyThread(QThread):
 
         # distance sensor
         self.sensor = DistanceSensor(27, 17)
-        
+
     def run(self):
         global sensor_num
         while True:
-            if sensor_num == 1: # distance sensor
+            if sensor_num == 1:  # distance sensor
                 print(self.sensor.distance)
                 time.sleep(0.5)
                 if self.sensor.distance < 0.15:
@@ -409,12 +405,12 @@ class MyThread(QThread):
                     if self.sensor.distance < 0.15:
                         self.distanceSignal.emit(-1)
                         sensor_num = 2
-                
+
             elif sensor_num == 2:
                 # RFID recognition
                 self.id, self.text = self.rfid_sensor.read()
                 print(hex(self.id))
-                self.mySignal.emit(int(self.id))
+                self.mySignal.emit(str(hex(self.id)))
                 sensor_num = 3
 
     def stop(self):
@@ -422,13 +418,14 @@ class MyThread(QThread):
         self.quit()
         self.wait(1000)
 
+
 # guide display thread
 class toggleThread(QThread):
     mySignal = Signal(int)
 
     def __init__(self):
         super().__init__()
-    
+
     def run(self):
         global toggle_flag
         while True:
@@ -436,12 +433,12 @@ class toggleThread(QThread):
             print("toggle_flag : ", toggle_flag)
             if toggle_flag == 1:
                 self.mySignal.emit(1)
-            
 
     def stop(self):
         self.working = False
         self.quit()
         self.wait(1000)
+
 
 class socketThread(QThread):
     socketSignal = Signal(int)
@@ -458,7 +455,7 @@ class socketThread(QThread):
             while socketFlag == 0:
                 time.sleep(1)
                 pass
-            
+
             # client socket
             self.client_soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.client_soc.connect((self.HOST, self.PORT))
@@ -466,7 +463,7 @@ class socketThread(QThread):
 
             data = self.client_soc.recv(1024)
 
-            if data.decode() == '-1': # yolo error
+            if data.decode() == '-1':  # yolo error
                 print("[ERROR] YOLO SERVER ERROR")
                 self.socketSignal.emit(5)
                 time.sleep(2)
@@ -479,9 +476,9 @@ class socketThread(QThread):
                 print(item_list)
 
             # 적합인지 부적합인지
-            x_yes = False # label 'X' 있는지
-            collection_flag = True # 적합 : True, 부적합 : False
-            item_empty = True # item_list is empty
+            x_yes = False  # label 'X' 있는지
+            collection_flag = True  # 적합 : True, 부적합 : False
+            item_empty = True  # item_list is empty
 
             plasticTotal = 0
             plasticOk = 0
@@ -496,7 +493,7 @@ class socketThread(QThread):
 
                 for key, val in (item_list[j].items()):
                     label = trash_label[item_eng[key]]
-                    if j == 0: # plastic
+                    if j == 0:  # plastic
                         plasticTotal += val
                     elif j == 1:
                         recycleTotal += val
@@ -506,19 +503,18 @@ class socketThread(QThread):
                         print(item_kor[item_eng[key]] + " " + str(val) + " => " + label)
                         wrong_list[j] += item_kor[item_eng[key]] + "\n"
                         x_yes = True
-                    elif (label == 'P' and  j != 0) or (label == 'R' and j != 1) or (label == 'T' and j != 2):
+                    elif (label == 'P' and j != 0) or (label == 'R' and j != 1) or (label == 'T' and j != 2):
                         # 틀린 것만 출력
                         print(item_kor[item_eng[key]] + " " + str(val) + " => " + label)
                         wrong_list[j] += item_kor[item_eng[key]] + "\n"
                         collection_flag = False
-                    else: # right
+                    else:  # right
                         if j == 0:
                             plasticOk += val
                         elif j == 1:
                             recycleOk += val
 
-
-            # x 있을 때 
+            # x 있을 때
             if x_yes == True:
                 print("X YES")
                 collection_flag = False
@@ -533,7 +529,7 @@ class socketThread(QThread):
                 print("WRONG")
                 # 부적합
                 self.socketSignal.emit(1)
-                
+
             else:
                 print("RIGHT")
                 # 적합 -> 수거
@@ -547,6 +543,7 @@ class socketThread(QThread):
         self.working = False
         self.quit()
         self.wait(1000)
+
 
 app = QApplication()
 win = MyApp()
