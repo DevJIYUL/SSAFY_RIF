e2e04c05cbd15bec47c2435b3ee5115a62397750
vinitus <tlsdnrng@gmail.com>
2023-02-15 16:51:45 +0900

    remove console setting

--- frontend/rif/src/App.js
+++ frontend/rif/src/App.js
@@ -2,11 +2,6 @@ import { RouterProvider } from "react-router-dom";
 import router from "./router";
 
 function App() {
-  if (process.env.NODE_ENV === "production") {
-    console.log = function () {};
-    console.warn = function () {};
-    console.warn = function () {};
-  }
   return <RouterProvider router={router} />;
 }
 
